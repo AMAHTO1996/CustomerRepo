ca09e1546fc17a975819cc5eb5f163ecf2cb6e3b
Amitkumar Mahto <99329609+AMAHTO1996@users.noreply.github.com>
2022-10-26 17:43:36 +0530

    Update CustomerController.java

--- src/main/java/com/amit/customer/controller/CustomerController.java
+++ src/main/java/com/amit/customer/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.amit.customer.controller;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
