46ae0bd6e41088f195048557db6f9229aca4395a
Amitkumar Mahto <99329609+AMAHTO1996@users.noreply.github.com>
2022-10-26 17:49:59 +0530

    Update MusicDO.java

--- src/main/java/com/amit/customer/entities/MusicDO.java
+++ src/main/java/com/amit/customer/entities/MusicDO.java
@@ -44,6 +44,109 @@ public class MusicDO {
 		this.field4 = field4;
 		this.field5 = field5;
 	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getBackground_image() {
+		return background_image;
+	}
+	public void setBackground_image(String background_image) {
+		this.background_image = background_image;
+	}
+	public boolean isVideo() {
+		return isVideo;
+	}
+	public void setVideo(boolean isVideo) {
+		this.isVideo = isVideo;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public int getLikeCount() {
+		return likeCount;
+	}
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+	public int getDisLikeCount() {
+		return disLikeCount;
+	}
+	public void setDisLikeCount(int disLikeCount) {
+		this.disLikeCount = disLikeCount;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public String getVideo_path() {
+		return video_path;
+	}
+	public void setVideo_path(String video_path) {
+		this.video_path = video_path;
+	}
+	public String getField1() {
+		return field1;
+	}
+	public void setField1(String field1) {
+		this.field1 = field1;
+	}
+	public String getField2() {
+		return field2;
+	}
+	public void setField2(String field2) {
+		this.field2 = field2;
+	}
+	public String getField3() {
+		return field3;
+	}
+	public void setField3(String field3) {
+		this.field3 = field3;
+	}
+	public String getField4() {
+		return field4;
+	}
+	public void setField4(String field4) {
+		this.field4 = field4;
+	}
+	public String getField5() {
+		return field5;
+	}
+	public void setField5(String field5) {
+		this.field5 = field5;
+	}
 	
 	
 	
