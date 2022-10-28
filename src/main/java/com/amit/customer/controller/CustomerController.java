68a7d16f33ed2734ed8006881c1d399e39f0f51e
Amitkumar Mahto <99329609+AMAHTO1996@users.noreply.github.com>
2022-10-28 17:34:38 +0530

    Update CustomerController.java

--- src/main/java/com/amit/customer/controller/CustomerController.java
+++ src/main/java/com/amit/customer/controller/CustomerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import music.api.entity.MusicDO;
 import com.amit.customer.entities.UserDO;
 import com.amit.customer.service.CustomerService;
@@ -37,22 +40,26 @@ public class CustomerController {
 		return customerService.findById(Id);
 	}
 	
-		@GetMapping("/allmusic")
-	public List<MusicDO> getAllMusic(){
-		MusicDO musicDO1 = new MusicDO(1, "Dil Ka Rishta - Jhankar ", "Tum Na Aaye Ho Tum Naa Aaoge", "https://i.ytimg.com/vi/u5MN540x3Tk/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBFm7IRIOL7RrmB4KqJIu1jKGQwAg", "background_image", false,
-				"/", "3.50", 178, 12, 7.8, "/",
-				"field1", "field2", "field3", "field4", "field5");
-		MusicDO musicDO2 = new MusicDO(2, "title", "description", "https://github.com/AMAHTO1996/amitkumar-portfolio-web/blob/master/images/image_3.jpg", "background_image", false,
-				"/", "4.50", 178, 12, 7.8, "/",
-				"field1", "field2", "field3", "field4", "field5");
-		MusicDO musicDO3 = new MusicDO(3, "title", "description", "https://github.com/AMAHTO1996/amitkumar-portfolio-web/blob/master/images/image_3.jpg", "background_image", false,
-				"/", "6.50", 178, 12, 7.8, "/",
-				"field1", "field2", "field3", "field4", "field5");
+	@GetMapping("/allmusic")
+	public ResponseEntity<List<MusicDO>> getAllMusic(@RequestParam String api_key){
 		List<MusicDO> l1 = new ArrayList<>();
-		l1.add(musicDO1);
-		l1.add(musicDO2);
-		l1.add(musicDO3);
-		return l1;
+		if (api_key.equalsIgnoreCase("abcdefgh")) {
+			MusicDO musicDO1 = new MusicDO(1, "Dil Ka Rishta - Jhankar ", "Tum Na Aaye Ho Tum Naa Aaoge", "https://i.ytimg.com/vi/u5MN540x3Tk/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBFm7IRIOL7RrmB4KqJIu1jKGQwAg", "background_image", false,
+					"/", "3.50", 178, 12, 7.8, "/",
+					"field1", "field2", "field3", "field4", "field5");
+			MusicDO musicDO2 = new MusicDO(1, "title", "description", "https://github.com/AMAHTO1996/amitkumar-portfolio-web/blob/master/images/image_3.jpg", "background_image", false,
+					"/", "4.50", 178, 12, 7.8, "/",
+					"field1", "field2", "field3", "field4", "field5");
+			MusicDO musicDO3 = new MusicDO(1, "title", "description", "https://github.com/AMAHTO1996/amitkumar-portfolio-web/blob/master/images/image_3.jpg", "background_image", false,
+					"/", "6.50", 178, 12, 7.8, "/",
+					"field1", "field2", "field3", "field4", "field5");
+			l1.add(musicDO1);
+			l1.add(musicDO2);
+			l1.add(musicDO3);
+			return new ResponseEntity<>(l1,HttpStatus.OK);
+		}
+		return new ResponseEntity<>(l1,HttpStatus.OK);
+
 		
 	}
 
